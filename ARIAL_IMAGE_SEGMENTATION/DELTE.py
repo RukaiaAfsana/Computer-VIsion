0f6d540bf394f1b0c982e9283d56aca3ce4f5cc4
RukaiaAfsana <rabeyaafsana@gmail.com>
2025-02-01 20:01:40 +0600

    delete
    some  duplicated files

--- ARIAL_IMAGE_SEGMENTATION/DELTE.py
+++
@@ -1,7 +0,0 @@
-import torch
-DEVICE = "cuda"
-test_images = torch.rand(4, 3, 256, 256).to(DEVICE)  # Random images
-test_masks = torch.rand(4, 1, 256, 256).to(DEVICE)   # Random masks
-
-logits, loss = model(test_images, test_masks)
-print(f"Test loss: {loss.item()}")
